b972b3b077b5d16c605476d1822002b288980331
Nurislam Alimov <nalimov@sardinasystems.com>
2016-05-29 19:19:41 +0300

    refractoring

--- neural_networks.py
+++ neural_networks.py
@@ -1,34 +1,36 @@
-# -*- coding: utf-8 -*- 
-import numpy as np
+# -*- coding: utf-8 -*-
+
 
 def prepare_data(filename):
-	f = open(filename, 'r')
-	train_X = []
-	train_Y = []
+    f = open(filename, 'r')
+    train_X = []
+    train_Y = []
 
-	test_X = []
-	test_Y = []
+    test_X = []
+    test_Y = []
 
-	readlines = f.readlines()
-	limiter_train = 1072
+    readlines = f.readlines()
+    limiter_train = 1072
 
-	for i in xrange(limiter_train):
-		train_X.append([])
-		readlines_split = readlines[i].split(";")
-		for j in readlines_split:
-	    	    if (j == readlines_split[-1]): None
-	    	    else:		
-	       	       train_X[i].append(float(j))
-		train_Y.append(int(readlines_split[-1][0:len(readlines_split)-1]))
+    for i in xrange(limiter_train):
+        train_X.append([])
+        readlines_split = readlines[i].split(";")
+        for j in readlines_split:
+            if (j == readlines_split[-1]):
+                None
+            else:
+                train_X[i].append(float(j))
+        train_Y.append(int(readlines_split[-1][0:len(readlines_split) - 1]))
 
-	count = 0
-	for i in xrange(limiter_train - 1, len(readlines)):
-		test_X.append([])
-		readlines_split = readlines[i].split(";")
-		for j in readlines_split:
-	    	    if (j == readlines_split[-1]): None
-	    	    else:		
-	       	       test_X[count].append(float(j))
-		test_Y.append(int(readlines_split[-1][0:len(readlines_split)-1]))
-		count = count + 1
-	return train_X, train_Y, test_X, test_Y
+    count = 0
+    for i in xrange(limiter_train - 1, len(readlines)):
+        test_X.append([])
+        readlines_split = readlines[i].split(";")
+        for j in readlines_split:
+            if (j == readlines_split[-1]):
+                None
+            else:
+                test_X[count].append(float(j))
+        test_Y.append(int(readlines_split[-1][0:len(readlines_split) - 1]))
+        count = count + 1
+    return train_X, train_Y, test_X, test_Y
